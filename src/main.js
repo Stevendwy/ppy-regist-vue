5f03eede071d7d4f8c4f833bebe2cd8b3897eb29
ZweiZhao <ZweiZhao@foxmail.com>
2017-12-04 15:19:08 +0800

    只保留注册

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import App from './component/App.vue'
-import rHeader from './component/Header.vue'
-import Bottom from './component/Bottom.vue'
 
 new Vue({
   el: '#app',
   template: `
     <div>
-      <rHeader />
       <App />
-      <Bottom />
     </div>
   `,
   components: { App, rHeader, Bottom }
